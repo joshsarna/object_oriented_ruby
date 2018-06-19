abba163d42f18eb55a552be9b504aa5d89b0cf07
joshsarna <joshsarna@gmail.com>
2018-06-18 18:28:28 -0700

    add file to model bookstore

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+# Model a book store
+
+book1 = {:title => "The Andromeda Strain", :author => "Michael Crichton", :length => 285}
+book2 = {:title => "A Mad Desire to Dance", :author => "Elie Wiesel", :length => 274}
+book3 = {title: "The Protestant Ethic and the Spirit of Capitalism", author: "Max Weber", length: 125}
+
+p "#{book1[:title]} is a book by #{book1[:author]} that has #{book1[:length]} pages."
