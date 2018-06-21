fe2a5453526954ade3c9d7244cd5657eba2984b7
joshsarna <joshsarna@gmail.com>
2018-06-20 19:21:48 -0700

    answer question 2

--- pair_programming.rb
+++ pair_programming.rb
@@ -1,8 +1,21 @@
 # Question 2
 # a
 
-count = 0
-11.times do
-  p count
-  count += 1
+# count = 0
+# 11.times do
+#   p count
+#   count += 1
+# end
+
+# e
+
+# Sams_recipes = gets.chomp.to_i
+# Sallys_languages = gets.chomp.to_i
+# if Sams_recipes > 10 and Sallys_languages >= 5
+#   p "They should date"
+# end
+Sam_can_make = gets.chomp
+Sally_can_speak = gets.chomp
+if Sam_can_make == "crepes" or Sally_can_speak == "French"
+  p "They should marry"
 end
